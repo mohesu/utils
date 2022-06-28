6cf3186757d3e7bf695cb041a625966e0c74943d
arvindsangwan <rvndsngwn@gmail.com>
2022-06-29 00:18:11 +0530

    readme update, name change

--- lib/src/custom_stuff/custom_input_decoration.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-InputBorder customInputBorder(double radius) {
-  return OutlineInputBorder(
-    borderRadius: BorderRadius.all(Radius.circular(radius)),
-  );
-}
