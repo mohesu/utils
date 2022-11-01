034426a3e708d42db7aff8380ae02feda8b0d0fa
Arvind Sangwan <59143934+rvndsngwn@users.noreply.github.com>
2022-11-01 21:04:04 +0530

    update

--- lib/utils_widget.dart
+++ lib/utils_widget.dart
@@ -48,3 +48,4 @@ export 'src/ui/context_text_style.dart';
 export 'src/ui/context_theme_colors.dart';
 export 'src/ui/context_size.dart';
 export 'src/ui/responsive.dart';
+export 'src/ui/responsive.dart';
